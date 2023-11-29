5447715cf6f44b7d6307210416a958f232b09497
Jasonjohnson6935 <jason.johnson6935@gmail.com>
2023-11-29 17:33:59 -0600

    minor changes

--- index.js
+++ index.js
@@ -1,54 +1,82 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+document.addEventListener('DOMContentLoaded', function () {
+    const partyForm = document.getElementById('partyForm');
+    const partyList = document.getElementById('partyList');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+    // Fetch parties from the API on page load
+    fetchParties();
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/party-planner', { useNewUrlParser: true, useUnifiedTopology: true });
+    partyForm.addEventListener('submit', function (event) {
+        event.preventDefault();
 
-// Create a Party model
-const Party = mongoose.model('Party', {
-  name: String,
-  date: Date,
-  time: String,
-  location: String,
-  description: String,
-});
+        // Get party information from the form
+        const name = document.getElementById('name').value;
+        const date = document.getElementById('date').value;
+        const time = document.getElementById('time').value;
+        const location = document.getElementById('location').value;
+        const description = document.getElementById('description').value;
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+        // Send a POST request to add the new party
+        fetch('YOUR_API_ENDPOINT', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name,
+                date,
+                time,
+                location,
+                description,
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Add the new party to the list
+            addPartyToList(data);
+        })
+        .catch(error => console.error('Error:', error));
+    });
 
-// Set up the HTML view engine
-app.set('view engine', 'ejs');
+    function fetchParties() {
+        // Fetch parties from the API
+        fetch('YOUR_API_ENDPOINT')
+        .then(response => response.json())
+        .then(data => {
+            // Display parties in the list
+            data.forEach(party => {
+                addPartyToList(party);
+            });
+        })
+        .catch(error => console.error('Error:', error));
+    }
 
-// Routes
-app.get('/', async (req, res) => {
-  // Fetch all parties from the database
-  const parties = await Party.find({});
-  res.render('index', { parties });
-});
+    function addPartyToList(party) {
+        // Create list item for the party
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            <strong>${party.name}</strong><br>
+            Date: ${new Date(party.date).toLocaleDateString()}<br>
+            Time: ${party.time}<br>
+            Location: ${party.location}<br>
+            Description: ${party.description}<br>
+            <button onclick="deleteParty(${party.id})">Delete</button>
+        `;
 
-app.post('/add', async (req, res) => {
-  // Extract party information from the form
-  const { name, date, time, location, description } = req.body;
+        // Add the list item to the party list
+        partyList.appendChild(listItem);
+    }
 
-  // Create a new party and save it to the database
-  await Party.create({ name, date, time, location, description });
-
-  // Redirect to the home page
-  res.redirect('/');
-});
-
-app.post('/delete/:id', async (req, res) => {
-  // Extract the party ID from the URL parameter
-  const partyId = req.params.id;
-
-  // Remove the party from the database
-  await Party.findByIdAndRemove(partyId);
-
-  // Redirect to the home page
-  res.redirect('/');
+    function deleteParty(partyId) {
+        // Send a DELETE request to remove the party
+        fetch(`YOUR_API_ENDPOINT/${partyId}`, {
+            method: 'DELETE',
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Remove the party from the list
+            const listItem = document.querySelector(`li:has(button[onclick="deleteParty(${partyId})"])`);
+            listItem.remove();
+        })
+        .catch(error => console.error('Error:', error));
+    }
 });
