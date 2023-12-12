6552d3d9347320f8124f55191d9699c4fb7279ac
Jasonjohnson6935 <jason.johnson6935@gmail.com>
2023-12-12 12:33:26 -0600

    minor

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Get party information from the form
         const name = document.getElementById('name').value;
-        const date = document.getElementById('date').value;
+        const date = new Date(document.getElementById('date').value).toISOString();
         const time = document.getElementById('time').value;
         const location = document.getElementById('location').value;
         const description = document.getElementById('description').value;
@@ -36,6 +36,9 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(data => {
             // Add the new party to the list
             addPartyToList(data);
+
+            // Update the global variable with the new data
+            partiesData.push(data);
         })
         .catch(error => console.error('Error adding party:', error));
     });
@@ -48,7 +51,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log('API Response:', responseData);
 
                 if (Array.isArray(responseData.data)) {
-                    partiesData = responseData.data; // Update the global variable
+                    // Update the global variable with the fetched data
+                    partiesData = responseData.data;
+
                     responseData.data.forEach(party => {
                         addPartyToList(party);
                     });
@@ -62,18 +67,23 @@ document.addEventListener('DOMContentLoaded', function () {
     function addPartyToList(party) {
         // Create list item for the party
         const listItem = document.createElement('li');
-        listItem.innerHTML = `
-            <strong>${party.name}</strong><br>
-            Date: ${new Date(party.date).toLocaleDateString()}<br>
-            Time: ${party.time}<br>
-            Location: ${party.location}<br>
-            Description: ${party.description}<br>
-            <button onclick="deleteParty(${party.id})">Delete</button>
-        `;
+
+        // Create delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => deleteParty(party.id));
+
+        // Append elements to list item
+        listItem.appendChild(document.createElement('strong')).textContent = party.name;
+        listItem.appendChild(document.createElement('br')).textContent = `Date: ${new Date(party.date).toLocaleDateString()}`;
+        listItem.appendChild(document.createElement('br')).textContent = `Time: ${party.time}`;
+        listItem.appendChild(document.createElement('br')).textContent = `Location: ${party.location}`;
+        listItem.appendChild(document.createElement('br')).textContent = `Description: ${party.description}`;
+        listItem.appendChild(document.createElement('br'));
+        listItem.appendChild(deleteButton);
 
         // Add the list item to the party list
         partyList.appendChild(listItem);
-        
     }
 
     function deleteParty(partyId) {
@@ -84,11 +94,20 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.json())
         .then(data => {
             console.log('Delete API Response:', data);
-            
-            // Remove the party from the list
-            const listItem = document.querySelector(`li:has(button[onclick="deleteParty(${partyId})"])`);
-            listItem.remove();
+    
+            if (data.success) {
+                // Remove the party from the list
+                const listItem = document.querySelector(`li:has(button[onclick="deleteParty(${partyId})"])`);
+                listItem.remove();
+    
+                // Update the global variable by removing the deleted party
+                partiesData = partiesData.filter(party => party.id !== partyId);
+            } else {
+                console.error('Error deleting party:', data.error);
+            }
         })
         .catch(error => console.error('Error deleting party:', error));
     }
 });
+
+
