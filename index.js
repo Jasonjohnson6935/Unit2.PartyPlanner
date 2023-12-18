9cab689e00bcef4a1287df22d92ad6276857266a
Jasonjohnson6935 <jason.johnson6935@gmail.com>
2023-12-17 18:42:30 -0600

    minor changes

--- index.js
+++ index.js
@@ -1,113 +1,146 @@
-// Global variable to store fetched party data
-let partiesData = [];
+// Grabbing elements from the dom
+const inputNameElement = document.getElementById("inputName");
+const inputDateElement = document.getElementById("inputDate");
+const inputTimeElement = document.getElementById("inputTime");
+const inputLocationElement = document.getElementById("inputLocation");
+const inputDescriptionElement = document.getElementById("inputDescription");
+const addEventElement = document.getElementById("addEvent");
+const eventDisplayElement = document.getElementById("eventDisplay");
 
-document.addEventListener('DOMContentLoaded', function () {
-    const partyForm = document.getElementById('partyForm');
-    const partyList = document.getElementById('partyList');
+// tracking the value function
+function trackInputValue(inputElement) {
+  let currentInputValue = "";
+  inputElement.addEventListener("input", (e) => {
+    currentInputValue = e.target.value;
+    console.log("current value:", currentInputValue);
+  });
+  return currentInputValue;
+}
 
-    // Fetch parties from the API on page load
-    fetchParties();
+// Input Tracking Variables
+let currentNameValue = trackInputValue(inputNameElement);
+let currentDateValue = trackInputValue(inputDateElement);
+let currentTimeValue = trackInputValue(inputTimeElement);
+let currentLocationValue = trackInputValue(inputLocationElement);
+let currentDescriptionValue = trackInputValue(inputDescriptionElement);
 
-    partyForm.addEventListener('submit', handlePartyFormSubmit);
+// API Configuration
+const cohortName = "2310-FSA-ET-WEB-PT-SF-B-Jason/events";
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${cohortName}`;
+console.log(API_URL);
 
-    function handlePartyFormSubmit(event) {
-        event.preventDefault();
-        const partyData = getPartyFormData();
-        addParty(partyData);
-    }
+// State
+const state = {
+  events: [],
+};
 
-    function fetchParties() {
-        fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-PT-SF-B/events')
-            .then(response => response.json())
-            .then(responseData => {
-                if (Array.isArray(responseData.data)) {
-                    updatePartiesData(responseData.data);
-                    responseData.data.forEach(addPartyToList);
-                } else {
-                    console.error('Unexpected API response:', responseData);
-                }
-            })
-            .catch(error => console.error('Error fetching parties:', error));
-    }
+// Fetch Events Function
+async function getEvents() {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    state.events = data.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
-    function addParty(partyData) {
-        fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-PT-SF-B/events', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(partyData),
-        })
-        .then(response => response.json())
-        .then(data => {
-            addPartyToList(data);
-            updatePartiesData([...partiesData, data]);
-        })
-        .catch(error => console.error('Error adding party:', error));
-    }
+// Render Events Function
+function renderEvents() {
+  console.log(state);
+  if (!state.events.length) {
+    eventDisplayElement.innerHTML = "<li> No Events. </li>";
+    return;
+  }
 
-    function addPartyToList(party) {
-        const listItem = createPartyListItem(party);
-        partyList.appendChild(listItem);
-    }
+  const eventCards = state.events.map((event) => {
+    const ul = document.createElement("ul");
+    const button = document.createElement("button");
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      const eventId = event.id;
+      try {
+        deleteEvent(eventId);
+      } catch (error) {
+        console.error("Error", error.message);
+      }
+    });
 
-    function createPartyListItem(party) {
-        const listItem = document.createElement('li');
-        listItem.appendChild(createElementWithText('strong', party.name));
-        listItem.appendChild(createElementWithText('br', `Date: ${new Date(party.date).toLocaleDateString()}`));
-        listItem.appendChild(createElementWithText('br', `Time: ${party.time}`));
-        listItem.appendChild(createElementWithText('br', `Location: ${party.location}`));
-        listItem.appendChild(createElementWithText('br', `Description: ${party.description}`));
-        listItem.appendChild(createElementWithText('br'));
-        listItem.appendChild(createDeleteButton(party.id));
-        return listItem;
-    }
+    button.innerHTML = "Delete Event";
+    ul.innerHTML = `
+      <h2>${event.name}</h2>
+      <ul>${event.date}</ul>
+      <ul>${event.description}</ul>`;
+    ul.appendChild(button);
 
-    function createElementWithText(tag, text) {
-        const element = document.createElement(tag);
-        element.textContent = text;
-        return element;
-    }
+    return ul;
+  });
 
-    function createDeleteButton(partyId) {
-        const deleteButton = createElementWithText('button', 'Delete');
-        deleteButton.addEventListener('click', () => deleteParty(partyId));
-        return deleteButton;
-    }
+  eventDisplayElement.replaceChildren(...eventCards);
+}
 
-    function deleteParty(partyId) {
-        fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-PT-SF-B/events/${partyId}`, {
-            method: 'DELETE',
-        })
-        .then(response => {
-            if (response.ok) {
-                removePartyFromList(partyId);
-                updatePartiesData(partiesData.filter(party => party.id !== partyId));
-            } else {
-                console.error('Error deleting party:', response.error);
-            }
-        })
-        .catch(error => console.error('Error deleting party:', error));
-    }
+// Delete Event Function, for some reason you need to press it twice. 
+async function deleteEvent(eventId) {
+  const deleteAPI = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-PT-SF-B-Jason/events/${eventId}`;
+  try {
+    const response = await fetch(deleteAPI, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    console.log('Event deleted:', data);
 
-    function removePartyFromList(partyId) {
-        const listItem = Array.from(partyList.children).find(item => item.querySelector(`button[data-party-id="${partyId}"]`));
-        if (listItem) {
-            listItem.remove();
-        }
-    }
+    await getEvents();
+    renderEvents();
+  } catch (error) {
+    console.error('Error', error.message);
+  }
+}
 
-    function getPartyFormData() {
-        return {
-            name: document.getElementById('name').value,
-            date: new Date(document.getElementById('date').value).toISOString(),
-            time: document.getElementById('time').value,
-            location: document.getElementById('location').value,
-            description: document.getElementById('description').value,
-        };
-    }
+// Initial Fetch and Render
+(async () => {
+  await getEvents();
+  renderEvents();
+})();
 
-    function updatePartiesData(newData) {
-        partiesData = newData;
-    }
+addEventElement.addEventListener("click", async (e) => {
+  e.preventDefault(); 
+const newEvent = {
+  name: inputNameElement.value,
+  date : new Date(inputDateElement.value).toISOString(),
+  location: inputLocationElement.value,
+  description: inputDescriptionElement.value,
+};
+  await addEvent(newEvent);
+
 });
+
+
+async function addEvent(newObj){
+try{
+  console.log(newObj,API_URL)
+  const response = await fetch(API_URL,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newObj),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to add event. Status: ${response.status}`);
+  }
+const newObjData = await response.json();
+console.log(newObjData)
+await getEvents();
+  renderEvents();
+
+return newObjData;
+
+} catch (error){
+  console.error('Error:', error.message);
+}
+
+}
